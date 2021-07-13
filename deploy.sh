4d3f3cea8497a3eb8bd572afcda53ed824b0a081
ufaboy <39663655+ufaboy@users.noreply.github.com>
2021-07-13 12:30:34 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ufaboy/game-memory.git developer:gh-pages
+git push -f git@github.com:ufaboy/game-memory.git developer
 
 cd -
