50e0014772e8eb28a65b1cb8272e4c1f3d0f3afe
AzaT <39663655+ufaboy@users.noreply.github.com>
2021-07-13 17:12:56 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ufaboy/game-memory-example.git developer
+git push -f git@github.com:ufaboy/game-memory-example.git master
 
 cd -
